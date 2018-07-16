1e0f61f3b3301de27549b35cc1be2b7fe9a1ea01
Yu He <yuhe00@gmail.com>
2018-07-16 14:46:49 +0000

    Implement LED light control for DNT scenes

---
+++ dnt_scene1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import time
+import math
+import random
+
+import apa102
+
+NUM_LEDS = 10
+
+def main():
+    try:
+        strip = apa102.APA102(num_led = NUM_LEDS,
+                              global_brightness = 255,
+                              mosi = 10,
+                              sclk = 11,
+                              order = 'rbg')
+
+        trains = [] # Trains are (length, position, speed)
+
+        currentTime = time.time() # In seconds
+
+        while True:
+            deltaTime = time.time() - currentTime
+            currentTime = time.time()
+
+            trains = list(map(lambda (l, p, s): (l, p + deltaTime * s, s), trains))
+            trains = list(filter(lambda (l, p, _): (int(p * (NUM_LEDS - 1)) - l) < NUM_LEDS, trains))
+
+            if random.random() > 0.998:
+                length = random.randrange(0, 10)
+                speed = random.randrange(2.0, 5.0)
+                trains.append((length, 0, speed))
+
+            for i in range(0, (NUM_LEDS - 1)):
+                strip.set_pixel(i, 255, 255, 255, 255)
+
+            for (l, p, s) in trains:
+                for i in range(0, l):
+                    index = int(p * (NUM_LEDS - 1))
+                    strip.set_pixel(max(index - i, 0), 0, 0, 0, 0)
+
+            strip.show();
+
+            time.sleep(1 / 60)
+
+    except KeyboardInterrupt:
+        print('Interrupted...')
+
+    strip.clear_strip()
+    strip.cleanup()
+
+if __name__ == "__main__":
+    main()
---
+++ dnt_scene2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import time
+import math
+import random
+
+import apa102
+
+NUM_LEDS = 10
+
+def main():
+    try:
+        strip = apa102.APA102(num_led = NUM_LEDS,
+                              global_brightness = 255,
+                              mosi = 10,
+                              sclk = 11,
+                              order = 'rbg')
+
+        currentTime = time.time() # In seconds
+
+        while True:
+            deltaTime = time.time() - currentTime
+            currentTime = time.time()
+
+            for i in range(0, (NUM_LEDS - 1)):
+                n = int(((math.sin(currentTime) + 1) / 2) * 64) + 32
+                strip.set_pixel(i, n, n, n, n)
+
+            strip.show();
+
+            time.sleep(1 / 60)
+
+    except KeyboardInterrupt:
+        print('Interrupted...')
+
+    strip.clear_strip()
+    strip.cleanup()
+
+if __name__ == "__main__":
+    main()
